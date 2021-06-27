ddeebfaa7b1fe03f38d07e6484b6c6d80e666a38
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-27 11:34:59 -0300

    Create Team model

--- api/models.py
+++ api/models.py
@@ -7,3 +7,15 @@ class Core(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Team(Core):
+    team_name = models.CharField(max_length=250, unique=True)
+    noc = models.CharField(max_length=3)
+
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+
+    def __str__(self):
+        return self.team_name
