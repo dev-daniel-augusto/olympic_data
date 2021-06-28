46fed8a176ba75ebcfb20d373608dbd7487f3699
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-28 14:14:38 -0300

    Create a viewsets class for each model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import (
+    Team,
+    Sport,
+    Event,
+    Athlete,
+    Game,
+    Medal,
+)
+from .serializers import (
+    TeamSerializer,
+    SportSerializer,
+    EventSerializer,
+    AthleteSerializer,
+    GameSerializer,
+    MedalSerializer,
+)
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class SportViewSet(viewsets.ModelViewSet):
+    queryset = Sport.objects.all()
+    serializer_class = SportSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class AthleteViewSet(viewsets.ModelViewSet):
+    queryset = Athlete.objects.all()
+    serializer_class = AthleteSerializer
+
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+
+class MedalViewSet(viewsets.ModelViewSet):
+    queryset = Medal.objects.all()
+    serializer_class = MedalSerializer
