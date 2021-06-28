fffd666f8defcd4f720149df3fc3f1252c46ab11
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-28 16:23:14 -0300

    Add filter functionality to the views

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 
+from django_filters.rest_framework import DjangoFilterBackend
+
+
 from .models import (
     Team,
     Sport,
@@ -21,28 +24,71 @@ from .serializers import (
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = (
+        'team_name',
+        'noc',
+        'created',
+    )
 
 
 class SportViewSet(viewsets.ModelViewSet):
     queryset = Sport.objects.all()
     serializer_class = SportSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = (
+        'sport_name',
+        'created',
+    )
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = (
+        'event_name',
+        'created',
+    )
 
 
 class AthleteViewSet(viewsets.ModelViewSet):
     queryset = Athlete.objects.all()
     serializer_class = AthleteSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = (
+        'name',
+        'sex',
+        'age',
+        'height',
+        'weight',
+        'athlete_team',
+        'created',
+    )
 
 
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = (
+        'year',
+        'season',
+        'city',
+        'sport',
+        'event',
+        'athletes',
+        'created',
+    )
 
 
 class MedalViewSet(viewsets.ModelViewSet):
     queryset = Medal.objects.all()
     serializer_class = MedalSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = (
+        'medal',
+        'event',
+        'winner',
+        'created',
+    )
