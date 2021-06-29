9cc5c1d8ee1b6b47595b333d6e1d2cc446a36f71
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-29 14:34:26 -0300

    Register the models on admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,95 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Team,
+    Sport,
+    Event,
+    Athlete,
+    Game,
+    Medal,
+)
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'team_name',
+        'noc',
+    )
+    search_fields = ('team_name', 'noc',)
+    list_filter = ('team_name',)
+
+
+@admin.register(Sport)
+class SportAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'sport_name',
+    )
+    search_fields = ('sport_name',)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'event_name',
+    )
+    search_fields = ('event_name',)
+
+
+@admin.register(Athlete)
+class AthleteAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'sex',
+        'age',
+        'height',
+        'weight',
+        'athlete_team',
+        'created',
+        'modified',
+    )
+    search_fields = (
+        'name',
+        'sex',
+        'age',
+        'height',
+        'weight',
+        'athlete_team'
+    )
+    list_filter = ('sex', 'athlete_team', 'age')
+
+
+@admin.register(Game)
+class Gamedmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'year',
+        'season',
+        'event',
+        'sport',
+    )
+    search_fields = (
+        'year',
+        'season',
+        'event',
+        'sport'
+    )
+    list_filter = ('season', 'sport')
+
+
+@admin.register(Medal)
+class MedalAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'medal',
+        'event',
+        'winner',
+        'created',
+        'modified',
+    )
+    search_fields = ('medal', 'event', 'winner')
+    list_filter = ('medal',)
